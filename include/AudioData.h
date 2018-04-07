5f1c7092b3b7df149c25ecde74907c07c71d30e4
cokpsz <cokpsz@gmail.com>
2018-04-07 16:38:58 +0700

    Add files via upload

---
+++ include/AudioData.h
@@ -0,0 +1,25 @@
+#include <SDL2/SDL.h>
+#include <string>
+#ifndef AUDIODATA_H
+#define AUDIODATA_H
+
+
+class AudioData
+{
+    public:
+        AudioData(std::string file_path);
+        virtual ~AudioData();
+        void setCallback(void* callback);
+        double getFreq();
+        void play();
+        void loadDevice();
+        Uint8* pos;
+        Uint32 length;
+        SDL_AudioSpec wavSpec;
+
+    private:
+
+        SDL_AudioDeviceID device;
+};
+
+#endif // AUDIODATA_H
